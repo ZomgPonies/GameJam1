a95a20099dfa6870ba4e3339099b20daf1c55996
PereKastor <constant.95@gmail.com>
2017-07-07 23:30:56 -0400

    Add mission to complete the "rite"

---
+++ Assets/Scripts/Mission.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+
+    public List<ObjectInteractive> m_objectList;  
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsObjectInMission(ObjectInteractive obj)
+    {
+        return m_objectList.Exists((ObjectInteractive intObj) => intObj.getID() == obj.getID());
+    }
+}
