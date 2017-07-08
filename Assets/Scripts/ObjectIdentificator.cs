338aaec375efcafaba6c7273f8d2323488e866e2
PereKastor <constant.95@gmail.com>
2017-07-07 23:30:42 -0400

    Add object interactive and identificator

---
+++ Assets/Scripts/ObjectIdentificator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectIdentificator : MonoBehaviour
+{
+
+    static int m_ObjectID;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_ObjectID = 0;
+    }
+
+    public int getNextObjectID()
+    {
+        return m_ObjectID++;
+    }
+
+}
---
+++ Assets/Scripts/ObjectInteractive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteractive : MonoBehaviour
+{
+
+    public ObjectIdentificator m_ObjectIdentificator;
+
+    private int m_ID;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_ID = m_ObjectIdentificator.getNextObjectID();
+        Debug.Log(name + " is with ID : " + m_ID);
+    }
+
+    public int getID()
+    {
+        return m_ID;
+    }
+
+    public void OnFailPickup()
+    {
+
+    }
+
+    public void OnPickupGood()
+    {
+
+    }
+}
