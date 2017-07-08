633dc43749f7b550ef36c7b88a91d31fbe0543d5
Kévin Duguay <kevin.duguay1@gmail.com>
2017-07-08 18:24:49 -0400

    Added SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    [SerializeField]
+    private AudioClip[] sounds;
+
+    public AudioClip[] Sounds
+    {
+        get { return sounds; }
+        private set { sounds = value; }
+    }
+
+    public AudioClip GetAudioClipByName(string audioClipName)
+    {
+        foreach (AudioClip sound in Sounds)
+        {
+            if (sound.name == audioClipName) return sound;
+        }
+
+        return null;
+    }
+}
