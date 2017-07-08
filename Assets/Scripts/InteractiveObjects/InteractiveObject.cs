45028af7c83ec2c5168a5e1891edd4160da258c6
PereKastor <constant.95@gmail.com>
2017-07-08 11:34:07 -0400

    Update Interactive Object files and delete useless

--- Assets/Scripts/InteractiveObjects/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObjects/InteractiveObject.cs
@@ -1,6 +1,33 @@
-﻿public class InteractiveObjectModel
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
 {
-    public int id;
-	public string name;
-    public string gameObjectName;
+    private InteractiveObjectModel m_model;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    public int getID()
+    {
+        return m_model.id;
+    }
+
+    public void setModel(InteractiveObjectModel model)
+    {
+        m_model = model;
+    }
+
+    public void OnFailPickup()
+    {
+        Debug.Log("MMmhh. The " + name + " object is not the right item to pick up.");
+    }
+
+    public void OnPickupGood()
+    {
+        Debug.Log("Good job, you succesly pick up the " + name + " object.");
+    }
 }
