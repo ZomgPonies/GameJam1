9e0ed0c9f164714a6e95c281298369cc32219e83
Kévin Duguay <kevin.duguay1@gmail.com>
2017-07-09 00:34:50 -0400

    Credit screen is accessable throw the intro menu

---
+++ Assets/Scripts/SceneManagers/CreditManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditManager : MonoBehaviour
+{
+    [SerializeField]
+    private string introSceneName;
+
+    private void Update()
+    {
+        if (Input.anyKey) SceneManager.LoadScene(introSceneName);
+    }
+}
---
+++ Assets/Scripts/SceneManagers/IntroManager.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button initiallySelectedButton;
+
+    [SerializeField]
+    private string gameSceneName;
+
+    [SerializeField]
+    private string creditSceneName;
+
+    [SerializeField]
+    private string helpSceneName;
+
+    private void Start()
+    {
+        initiallySelectedButton.Select();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void LoadCredit()
+    {
+        SceneManager.LoadScene(creditSceneName);
+    }
+
+    public void LoadHelp()
+    {
+        SceneManager.LoadScene(helpSceneName);
+    }
+}
